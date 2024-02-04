70e397b553fe117a4c7d66189deecb96e0328566
rashikasahu112 <rashikasahu162@gmail.com>
2024-02-04 11:25:53 +0530

    made changes for deployment

---
+++ Client/src/Action.js
@@ -0,0 +1,13 @@
+const ACTIONS = {
+    JOIN : 'join',
+    JOINED : 'joined',
+    DISCONNECTED : 'disconnected',
+    CODE_CHANGE : 'code-change',
+    SYNC_CODE : 'sync-code',
+    LEAVE : 'leave', 
+    CONNECT_ERROR : 'connect_error',
+    CONNECT_FAILED : 'connect_failed'
+}
+
+//export default ACTIONS 
+module.exports = ACTIONS
--- Server/Server.js
+++ Server/Server.js
@@ -1,24 +1,30 @@
 const express = require('express')
 const {createServer} = require('http')
 const {Server} = require('socket.io')
-const ACTIONS = require('./src/Action')
+const ACTIONS = require('./Action')
 const path = require('path');
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 let clients = {}
 //app.use(express.static(path.join(__dirname, '../build')));
 //app.use(express.static('build'));
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname,'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.use((req, res) => {
+//     res.sendFile(path.join(__dirname,'build', 'index.html'));
+// });
+
+
+
 server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
+})
+app.get('/', (req, res) => {
+    res.json({"message" : "server running..."})
 })
 
 io.on('connection', (socket) => {
